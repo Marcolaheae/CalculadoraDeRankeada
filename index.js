b89a48cd7ac61df9b52d85a19edf1699fbb334a4
Marcolaheae <167900661+Marcolaheae@users.noreply.github.com>
2024-10-10 14:36:18 -0300

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+let vitoria = 60
+
+let derrota = 3
+
+let saldodeVitoria = (vitoria - derrota)
+
+  if (saldodeVitoria <= 10){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Ferro')
+  }
+  
+  if (saldodeVitoria >= 11 && saldodeVitoria <= 20){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Bronze')
+  }
+  
+  if (saldodeVitoria >= 21 && saldodeVitoria <= 50){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Prata')
+  }
+  
+  if (saldodeVitoria >= 51 && saldodeVitoria <= 80){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Ouro')
+  }
+  
+  if (saldodeVitoria >= 81 && saldodeVitoria <= 90){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Diamante')
+  }
+  
+  if (saldodeVitoria >= 91 && saldodeVitoria <= 100){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Lendário') 
+  }
+  
+  if (saldodeVitoria >= 101){
+  console.log('O Herói tem de saldo ' + saldodeVitoria + ' está no nível de Imortal')
+  }
